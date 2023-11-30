752f4b0678f176e95dbf9ce9e4b549b18461ee35
Christian Onoh <chibyk5000@gmail.com>
2023-11-30 23:46:18 +0100

    Add client import and update navigation links

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import styles from "@/styles";
 import Logo from "@/public/icons/ngworocks_logo.svg";
 import Image from "next/image";
@@ -5,13 +6,20 @@ import Link from "next/link";
 import { useState } from "react";
 import { Fade as Hamburger } from "hamburger-react";
 import { cx } from "@/utils";
+import { useScrollPosition } from "../hooks";
+import { usePathname } from "next/navigation";
+import { mobileNavLinks, navLinks1, navLinks2 } from "@/constants";
 
 const Header = () => {
   const [toggled, setToggled] = useState(false);
+  const scrollPosition = useScrollPosition()
+  const pathname = usePathname();
+
+
   return (
-    <header className={`${styles.xPaddings} py-6 relative bg-white `}>
+    <header className={cx(scrollPosition > 0 ? 'shadow' : 'shadow-none', 'sticky -top-2 w-full z-30 py-6 bg-white')}>
       <nav
-        className={`${styles.innerWidth} max-w-7xl mx-auto flex justify-between items-center text-lg  text-dark capitalize font-semi-bold`}
+        className='max-w-7xl px-5 sm:px-16 font-semibold mx-auto flex justify-between relative items-center text-lg  text-dark capitalize font-semi-bold'
       >
         <Link
           href="/"
@@ -22,40 +30,32 @@ const Header = () => {
 
         {/* Desktop Navigation  */}
         <div className="hidden lg:flex justify-between items-center gap-4 lg:gap-6">
-          <Link
-            href="/"
-            className="hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
-          >
-            home
-          </Link>
-          <Link
-            href="/"
-            className="hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
-          >
-            about
-          </Link>
-          <Link
-            href="/"
-            className="hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
-          >
-            services
-          </Link>
+          {
+            navLinks1.map((link, index) => (
+              <Link
+                key={index}
+                href={link.url}
+                className={cx(pathname === link.url ? 'bg-accent/10' : 'text-dark', 'hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105')}
+              >
+                {link.name}
+              </Link>
+            ))
+          }
         </div>
         <div
           className={`hidden lg:flex justify-between items-center gap-4 lg:gap-6`}
         >
-          <Link
-            href="/"
-            className="hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
-          >
-            blog
-          </Link>
-          <Link
-            href="/"
-            className="hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
-          >
-            contact
-          </Link>
+          {
+            navLinks2.map((link, index) => (
+              <Link
+                key={index}
+                href={link.url}
+                className={cx(pathname === link.url ? 'bg-accent/10' : 'text-dark', 'hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105')}
+              >
+                {link.name}
+              </Link>
+            ))
+          }
           <Link
             href="/"
             className="bg-dark text-white py-2 px-4 rounded-lg transition-all duration-200 ease-in-out hover:bg-white hover:text-dark border border-dark"
@@ -67,7 +67,7 @@ const Header = () => {
         {/* Mobile Navigation  */}
         <div className="flex items-center gap-6 lg:hidden">
           <Link
-            href="/"
+            href="/blog"
             className="hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
           >
             blog
@@ -80,26 +80,19 @@ const Header = () => {
             label="menu"
           />
           </div>
-            <div className={`${toggled ? 'scale-y-100 origin-top' : 'scale-y-0 origin-top'} flex flex-col w-full bg-white shadow-sm absolute z-30 left-0 h-[384px] justify-center lg:hidden transform transition-transform duration-300 ease-in-out top-0`}>
+            <div className={cx(toggled ? 'scale-y-100 origin-top' : 'scale-y-0 origin-top', 'flex flex-col w-full bg-white shadow-sm absolute z-30 left-0 h-[384px] justify-center lg:hidden transform transition-transform duration-300 ease-in-out top-0')}>
             <div className="py-32 px-8 max-w-[287px] mx-auto text-base gap-4 h-full flex flex-col w-full items-center text-black capitalize font-semibold">
-              <Link
-                href="/"
-                className="text-center  hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
-              >
-                about
-              </Link>
-              <Link
-                href="/"
-                className="text-center  hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
-              >
-                services
-              </Link>
-              <Link
-                href="/"
-                className="text-center  hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
-              >
-                contact
-              </Link>
+              {
+                mobileNavLinks.map((link, index) => (
+                  <Link
+                    key={index}
+                    href={link.url}
+                    className="text-center  hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
+                  >
+                    {link.name}
+                  </Link>
+                ))
+              }
             </div>
             </div>
         </div>
