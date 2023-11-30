3d0a746f554cac2ae28a34b1c1b303e4a123fb80
Christian Onoh <chibyk5000@gmail.com>
2023-11-30 23:49:07 +0100

    Update global CSS styles

--- styles/index.ts
+++ styles/index.ts
@@ -2,7 +2,7 @@ const styles = {
   innerWidth: '2xl:max-w-[1280px] w-full mx-auto',
   interWidth: 'lg:w-[80%] w-[100%]',
 
-  paddings: 'sm:p-16 xs:p-8 px-6 py-12',
+  paddings: 'sm:p-16 xs:p-8 px-5 py-12',
   yPaddings: 'sm:py-16 xs:py-8 py-12',
   xPaddings: 'sm:px-16 px-6',
   topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
@@ -12,6 +12,9 @@ const styles = {
   flexStart: 'flex justify-start items-start',
   flexEnd: 'flex justify-end',
   navPadding: 'pt-[98px]',
+
+  // effects
+  underline: 'bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500',
 };
 
 export default styles;
