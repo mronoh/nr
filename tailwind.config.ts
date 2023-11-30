b3723e2bedff3b14a07fbd07212f37e3e696a4ea
Christian Onoh <chibyk5000@gmail.com>
2023-11-30 23:44:48 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './sections/**/*.{html,js,ts,jsx,tsx,mdx}',
-    './styles/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        dark: '#062817',
-        light: '#fff',
-        accent: '#007425',
+        dark: "#062817",
+        light: "#fff",
+        accent: "#007425",
+        green: "#15B825",
+        grayLight: "#BDBDBD",
+        bgColor: "#f7f7f7"
       },
       fontFamily: {
-        mont: ['var(--font-montserrat)'],
+        mont: ["var(--font-montserrat)"],
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+      screens: {
+        'xs': '425px',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
