d7a74564c72c6a6dcf0e525de4932a8a22ece250
Christian Onoh <chibyk5000@gmail.com>
2023-11-30 16:00:00 +0100

    Add mobile navigation to header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -63,6 +63,46 @@ const Header = () => {
             get in touch
           </Link>
         </div>
+
+        {/* Mobile Navigation  */}
+        <div className="flex items-center gap-6 lg:hidden">
+          <Link
+            href="/"
+            className="hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
+          >
+            blog
+          </Link>
+          <div className="z-50 text-dark hover:bg-accent/10 rounded-full">
+            <Hamburger
+            toggle={setToggled}
+            toggled={toggled}
+            size={27}
+            label="menu"
+          />
+          </div>
+            <div className={`${toggled ? 'scale-y-100 origin-top' : 'scale-y-0 origin-top'} flex flex-col w-full bg-white shadow-sm absolute z-30 left-0 h-[384px] justify-center lg:hidden transform transition-transform duration-300 ease-in-out top-0`}>
+            <div className="py-32 px-8 max-w-[287px] mx-auto text-base gap-4 h-full flex flex-col w-full items-center text-black capitalize font-semibold">
+              <Link
+                href="/"
+                className="text-center  hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
+              >
+                about
+              </Link>
+              <Link
+                href="/"
+                className="text-center  hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
+              >
+                services
+              </Link>
+              <Link
+                href="/"
+                className="text-center  hover:bg-accent/10 px-4 py-1.5 rounded-lg transition-all duration-200 ease-in-out hover:scale-105"
+              >
+                contact
+              </Link>
+            </div>
+            </div>
+        </div>
       </nav>
     </header>
   );
