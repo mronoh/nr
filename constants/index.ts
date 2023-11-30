de9e6187e059e0dfbcdea99a17f6430d89294853
Christian Onoh <chibyk5000@gmail.com>
2023-11-30 23:48:50 +0100

    Add footer pages, Nav link and achievements constants

--- constants/index.ts
+++ constants/index.ts
@@ -16,3 +16,96 @@ export const socials = [
     url: '/icons/facebook.svg',
   },
 ];
+
+export const footerPages = [
+  {
+    name: 'Home',
+    url: '/',
+  },
+  {
+    name: 'About',
+    url: '/about',
+  },
+  {
+    name: 'Contact',
+    url: '/contact',
+  },
+  {
+    name: 'Blog',
+    url: '/blog',
+  },
+  {
+    name: 'Achievements',
+    url: '/#achievements',
+  },
+];
+
+
+export const achievements = [
+  {
+    title: 'Location Discovered',
+    stat: '50+',
+  },
+  {
+    title: 'Covered on foot',
+    stat: '500+ ml',
+  },
+  {
+    title: 'Location Discovered',
+    stat: '50+',
+  },
+  {
+    title: 'Location Discovered',
+    stat: '50+',
+  },
+  {
+    title: 'Covered on foot',
+    stat: '500+ ml',
+  },
+  {
+    title: 'Location Discovered',
+    stat: '50+',
+  },
+]
+
+export const navLinks1 = [
+  {
+    name: 'Home',
+    url: '/',
+  },
+  {
+    name: 'About',
+    url: '/#about',
+  },
+  {
+    name: 'Services',
+    url: '/#services',
+  },
+  
+]
+
+export const navLinks2 = [
+  {
+    name: 'Blog',
+    url: '/blog',
+  },
+  {
+    name: 'Contact',
+    url: '/contact',
+  },
+]
+
+export const mobileNavLinks = [
+  {
+    name: 'About',
+    url: '/#about',
+  },
+  {
+    name: 'Services',
+    url: '/#services',
+  },
+  {
+    name: 'Contact',
+    url: '/contact',
+  },
+]
