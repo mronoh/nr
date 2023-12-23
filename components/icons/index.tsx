0ea2f86ec96ea527537447ab0dbc9c638c25bcb1
Christian Onoh <chibyk5000@gmail.com>
2023-12-23 16:01:34 +0100

    Add Hamburger component to icons

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -129,3 +129,40 @@ export const NgwoRocksLogo = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 )
+
+export const Hamburger = ({
+  handleToggle,
+  toggled
+}: {
+  handleToggle: Function
+  toggled: boolean
+}) => {
+  return (
+    <button
+      aria-label='Hamburger Menu Toggle Button'
+      className=' rounded-full p-2 transition-all duration-300 ease-in-out hover:bg-accent/10 lg:hidden'
+      onClick={() => handleToggle(toggled)}
+    >
+      <div className='ease flex h-6 w-6 cursor-pointer flex-col justify-center transition-all duration-300'>
+        <div className='flex h-4 flex-col justify-between'>
+          <span
+            style={{
+              transform: toggled ? 'rotate(-45deg) translate(-5px, 5px)' : ''
+            }}
+            className='ease h-0.5 w-full rounded bg-dark transition-all duration-200 dark:bg-light'
+          />
+          <span
+            style={{ opacity: toggled ? 0 : 1 }}
+            className='ease h-0.5 w-full rounded bg-dark transition-all duration-200 dark:bg-light'
+          />
+          <span
+            style={{
+              transform: toggled ? 'rotate(45deg) translate(-5px, -5px)' : ''
+            }}
+            className='ease h-0.5 w-full rounded bg-dark transition-all duration-200 dark:bg-light'
+          />
+        </div>
+      </div>
+    </button>
+  )
+}
