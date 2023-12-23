69a13be6788699d7288f992afed546e6469fb4fe
Christian Onoh <chibyk5000@gmail.com>
2023-12-23 19:08:26 +0100

    Update schema.ts to include tag schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 import blockContent from './schemas/blockContent'
-import category from './schemas/category'
 import post from './schemas/post'
 import author from './schemas/author'
+import tag from './schemas/tag'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
+  types: [post, author, tag, blockContent],
 }
