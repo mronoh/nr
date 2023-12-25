19403c560ce6fe130b3212df7d74d2de5b06b91f
Christian Onoh <chibyk5000@gmail.com>
2023-12-25 12:56:08 +0100

    Update FeaturedBlogs component to pass showCasePosts as props

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -4,17 +4,20 @@ import FeaturedBlogs from '@/components/home/FeaturedBlogs'
 import Achievements from '@/components/home/Achievements'
 import { sanityFetch } from '@/sanity/lib/fetch'
 import { SanityDocument } from 'next-sanity'
-import { postsQuery } from '@/sanity/lib/queries'
+import { featuredAndHomeCoverPostsQuery, postsQuery } from '@/sanity/lib/queries'
 import { Post } from '@/components/blog/BlogLayoutOne'
 
 export default async function Home() {
+  const showCasePosts = await sanityFetch<any>({
+    query: featuredAndHomeCoverPostsQuery
+  })
   return (
     <main
       className={` transition-background relative flex min-h-screen flex-col justify-center bg-cover`}
     >
       <HomePageCover />
       <WhoAreWe />
-      <FeaturedBlogs />
+      <FeaturedBlogs posts={showCasePosts.featuredPosts} />
       <Achievements />
     </main>
   )
