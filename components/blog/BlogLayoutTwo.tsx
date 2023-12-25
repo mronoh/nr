41be3431b53ddf130fd4cf4dd71618189a17a9ee
Christian Onoh <chibyk5000@gmail.com>
2023-12-25 04:14:24 +0100

    Refactor BlogLayoutTwo component to use Post object

--- components/blog/BlogLayoutTwo.tsx
+++ components/blog/BlogLayoutTwo.tsx
@@ -3,31 +3,34 @@ import Tag from '../shared/Tag'
 import Image from 'next/image'
 import { cx } from '@/utils'
 import styles from '@/styles'
-import { slug as slugger } from 'github-slugger'
-import { Blog } from './BlogLayoutOne'
+import { Post } from './BlogLayoutOne'
+import { urlForImage } from '@/sanity/lib/image'
 
-const BlogLayoutTwo = ({ blog }: { blog: Blog }) => {
+const BlogLayoutTwo = ({ post }: { post: Post }) => {
   return (
     <div className=''>
-      <div className='absolute left-0 top-0 z-0 h-full w-full rounded-xl bg-gradient-to-b from-transparent to-dark/90' />
-      <Image
-        src={blog.image.filePath.replace('../public', '')}
-        alt={blog.title}
-        placeholder='blur'
-        blurDataURL={blog.image.blurhashDataUrl}
-        width={blog.image.width}
-        height={blog.image.height}
-        className='h-full w-full rounded-xl object-cover object-center'
-      />
-      <div className='absolute bottom-0 z-20 p-4 text-light'>
-        <Tag
-          link={`/tags/${slugger(blog.tags[0])}`}
-          title={blog.tags[0]}
-          className='!border px-6 py-2 text-sm'
+      <div className='absolute left-0 top-0 z-10 h-full w-full rounded-xl bg-gradient-to-b from-transparent to-dark/90' />
+      {post?.mainImage && (
+        <Image
+          src={urlForImage(post.mainImage.image).url()}
+          alt={post.mainImage.alt ?? post?.title}
+          placeholder='blur'
+          blurDataURL={post.mainImage.lqip}
+          fill
+          className='h-full w-full rounded-xl -z-0 object-cover object-center'
         />
-        <Link href={blog.url} className='mt-6'>
+      )}
+      <div className='absolute bottom-0 z-20 p-4 text-light'>
+        {post?.tags && (
+          <Tag
+            link={``}
+            title={post?.tags[0].title || ''}
+            className='!border px-6 py-2 text-sm'
+          />
+        )}
+        <Link href={`/blog/${post?.slug}`} className='mt-6'>
           <h2 className='class="font-bold mt-4 text-2xl capitalize text-light'>
-            <span className={cx(styles.underline)}>{blog.title}</span>
+            <span className={cx(styles.underline)}>{post?.title}</span>
           </h2>
         </Link>
       </div>
