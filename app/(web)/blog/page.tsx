e3327eb2b6b6d69e1926a13018642b23f9a06e87
Christian Onoh <chibyk5000@gmail.com>
2023-12-25 13:13:47 +0100

    Refactor blog page component to fetch showcase posts from Sanity

--- app/(web)/blog/page.tsx
+++ app/(web)/blog/page.tsx
@@ -1,12 +1,19 @@
 import HomeCover from '@/components/blog/HomeCover'
 import RecentBlogs from '@/components/blog/RecentPosts'
 import FeaturedBlogs from '@/components/home/FeaturedBlogs'
+import { sanityFetch } from '@/sanity/lib/fetch'
+import {
+  featuredAndHomeCoverPostsQuery,
+} from '@/sanity/lib/queries'
 
-const BlogPage = () => {
+const BlogPage = async () => {
+  const showCasePosts = await sanityFetch<any>({
+    query: featuredAndHomeCoverPostsQuery
+  })
   return (
     <main>
-      <HomeCover />
-      <FeaturedBlogs />
+      <HomeCover post={showCasePosts.homeCoverPost} />
+      <FeaturedBlogs posts={showCasePosts.featuredPosts} />
       <RecentBlogs />
     </main>
   )
