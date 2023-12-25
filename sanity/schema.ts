87326612424ae99a59932e76c4cef31b50af2a2c
Christian Onoh <chibyk5000@gmail.com>
2023-12-25 13:11:17 +0100

    Add new schemas for showCasePost, featuredPost, and homeCoverPost

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -4,7 +4,10 @@ import blockContent from './schemas/blockContent'
 import post from './schemas/post'
 import author from './schemas/author'
 import tag from './schemas/tag'
+import showCasePost from './schemas/showCasePost'
+import featuredPost from './schemas/featuredPost'
+import homeCoverPost from './schemas/homeCoverPost'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, tag, blockContent],
+  types: [post, author, tag, blockContent, showCasePost, featuredPost, homeCoverPost],
 }
