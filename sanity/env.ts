7e53402b9686c9e820442a02ad384943b5b58c77
Christian Onoh <chibyk5000@gmail.com>
2023-12-25 15:01:38 +0100

    Add previewSecretId to env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,6 +11,8 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
+export const previewSecretId = process.env.NEXT_PUBLIC_SANITY_PREVIEW_SECRET;
+
 export const useCdn = false
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
