745243143a3e3a95a2404a223a3d24ecf547b79d
Christian Onoh <chibyk5000@gmail.com>
2023-12-25 04:14:09 +0100

    Add imports and change function to async in page.tsx

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -2,8 +2,12 @@ import HomePageCover from '@/components/home/HomePageCover'
 import WhoAreWe from '@/components/home/WhoAreWe'
 import FeaturedBlogs from '@/components/home/FeaturedBlogs'
 import Achievements from '@/components/home/Achievements'
+import { sanityFetch } from '@/sanity/lib/fetch'
+import { SanityDocument } from 'next-sanity'
+import { postsQuery } from '@/sanity/lib/queries'
+import { Post } from '@/components/blog/BlogLayoutOne'
 
-export default function Home() {
+export default async function Home() {
   return (
     <main
       className={` transition-background relative flex min-h-screen flex-col justify-center bg-cover`}
