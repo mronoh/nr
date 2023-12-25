ddb938b0cf8fad3439133e60688ad871d1a3398f
Christian Onoh <chibyk5000@gmail.com>
2023-12-25 04:14:14 +0100

    Refactor BlogLayoutOne component

--- components/blog/BlogLayoutOne.tsx
+++ components/blog/BlogLayoutOne.tsx
@@ -1,5 +1,8 @@
+import { urlForImage } from '@/sanity/lib/image'
 import styles from '@/styles'
 import { cx, formatDate } from '@/utils'
+import { SanityDocument } from 'next-sanity'
+import { Author } from 'next/dist/lib/metadata/types/metadata-types'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -17,37 +20,64 @@ export interface Blog {
     height: number
   }
 }
-const BlogLayoutOne = ({ blog }: { blog: Blog }) => {
+type Tag = {
+  title: string
+  slug: string
+}
+export interface Post extends SanityDocument {
+  title: string
+  slug: string
+  publishedAt: string
+  author: any
+  tags: Tag[]
+  body?: any
+  mainImage: {
+    image: any
+    lqip: string
+    alt: string
+    dimensions: {
+      width: number
+      height: number
+    }
+  }
+}
+const BlogLayoutOne = ({ post }: { post: Post }) => {
   return (
     <div className='grid items-center gap-4 text-dark lg:grid-cols-12'>
-      <Link
-        className='col-span-12 h-full overflow-hidden rounded-xl border border-dark lg:col-span-4'
-        href={blog.url}
-      >
-        <Image
-          src={blog.image.filePath.replace('../public', '')}
-          alt={blog.title}
-          placeholder='blur'
-          blurDataURL={blog.image.blurhashDataUrl}
-          width={blog.image.width}
-          height={blog.image.height}
-          className='aspect-square h-full w-full object-cover object-center transition-all duration-200 ease-in-out hover:scale-105'
-        />
-      </Link>
+      {post?.mainImage && post?.slug && (
+        <Link
+          className='col-span-12 h-full overflow-hidden rounded-xl border border-dark lg:col-span-4'
+          href={`/blog/${post.slug}`}
+        >
+          <Image
+            src={urlForImage(post.mainImage.image).url()}
+            alt={post.mainImage.alt ?? post?.title}
+            placeholder='blur'
+            blurDataURL={post.mainImage.lqip}
+            width={post.mainImage.dimensions?.width ?? 1200}
+            height={post.mainImage.dimensions?.height ?? 630}
+            className='aspect-square h-full w-full object-cover object-center transition-all duration-200 ease-in-out hover:scale-105'
+          />
+        </Link>
+      )}
       <div className='col-span-12 lg:col-span-8'>
         <span className='text-sm font-semibold uppercase text-accent'>
-          {blog.tags[0]}
+          {post?.tags && post.tags[0].title}
         </span>
-        <Link href={blog.url} className='mt-6'>
-          <h2 className='text-lg font-semibold capitalize text-dark'>
-            <span
-              className={cx(styles.underline, 'from-accent/50 to-accent/50')}
-            >
-              {blog.title}
-            </span>
-          </h2>
-        </Link>
-        <p className='text-gray text-base'>{formatDate(blog.publishedAt)}</p>
+        {post?.slug && (
+          <Link href={`/blog/${post.slug}`}>
+            <h2 className='text-lg font-semibold capitalize text-dark'>
+              <span
+                className={cx(styles.underline, 'from-accent/50 to-accent/50')}
+              >
+                {post.title}
+              </span>
+            </h2>
+          </Link>
+        )}
+        {post?.publishedAt && (
+          <p className='text-gray text-base'>{formatDate(post.publishedAt)}</p>
+        )}
       </div>
     </div>
   )
