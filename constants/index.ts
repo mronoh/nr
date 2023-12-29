b770b06c841b16775f3890090a40da8430f46ddd
Christian Onoh <chibyk5000@gmail.com>
2023-12-29 10:52:50 +0100

    Add images and update URLs in constants file

--- constants/index.ts
+++ constants/index.ts
@@ -1,111 +1,148 @@
+import bonfire from '@/public/images/bonfire.webp'
+import hiking from '@/public/images/hiking.webp'
+import carnival from '@/public/images/carnival.webp'
+import streaming from '@/public/images/streaming.webp'
+import tours from '@/public/images/tours.webp'
+
 export const socials = [
   {
     name: 'twitter',
-    url: '/icons/twitter.svg',
+    url: '/icons/twitter.svg'
   },
   {
     name: 'linkedin',
-    url: '/icons/linkedin.svg',
+    url: '/icons/linkedin.svg'
   },
   {
     name: 'instagram',
-    url: '/icons/instagram.svg',
+    url: '/icons/instagram.svg'
   },
   {
     name: 'facebook',
-    url: '/icons/facebook.svg',
-  },
-];
+    url: '/icons/facebook.svg'
+  }
+]
 
 export const footerPages = [
   {
     name: 'Home',
-    url: '/',
+    url: '/'
   },
   {
     name: 'About',
-    url: '/about',
+    url: '/about'
   },
   {
     name: 'Contact',
-    url: '/contact',
+    url: '/contact'
   },
   {
     name: 'Blog',
-    url: '/blog',
+    url: '/blog'
   },
   {
     name: 'Achievements',
-    url: '/#achievements',
-  },
-];
-
+    url: '/#achievements'
+  }
+]
 
 export const achievements = [
   {
     title: 'Location Discovered',
-    stat: '50+',
+    stat: '50+'
   },
   {
     title: 'Covered on foot',
-    stat: '500+ ml',
+    stat: '500+ ml'
   },
   {
     title: 'Location Discovered',
-    stat: '50+',
+    stat: '50+'
   },
   {
     title: 'Location Discovered',
-    stat: '50+',
+    stat: '50+'
   },
   {
     title: 'Covered on foot',
-    stat: '500+ ml',
+    stat: '500+ ml'
   },
   {
     title: 'Location Discovered',
-    stat: '50+',
-  },
+    stat: '50+'
+  }
 ]
 
 export const navLinks1 = [
   {
     name: 'Home',
-    url: '/',
+    url: '/'
   },
   {
     name: 'About',
-    url: '/#about',
+    url: '/#about'
   },
   {
     name: 'Services',
-    url: '/#services',
-  },
-  
+    url: '/#services'
+  }
 ]
 
 export const navLinks2 = [
   {
     name: 'Blog',
-    url: '/blog',
+    url: '/blog'
   },
   {
     name: 'Contact',
-    url: '/contact',
-  },
+    url: '/contact'
+  }
 ]
 
 export const mobileNavLinks = [
   {
     name: 'About',
-    url: '/#about',
+    url: '/#about'
   },
   {
     name: 'Services',
-    url: '/#services',
+    url: '/#services'
   },
   {
     name: 'Contact',
-    url: '/contact',
+    url: '/contact'
+  }
+]
+
+export const services = [
+  {
+    title: 'Hiking',
+    img: hiking,
+    link: '/services#hiking',
+    alt: 'Man hiking'
   },
+  {
+    title: 'Event Streaming',
+    img: streaming,
+    alt: 'Man smiling when videographing an event',
+    link: '/services#event-streaming'
+  },
+  {
+    title: 'Bonfire',
+    img: bonfire,
+    alt: 'Bonfire at night',
+    link: '/services#bonfire'
+  },
+  {
+    title: 'Unity Walk Carnival',
+    img: carnival,
+    alt: 'Ladies in green costumes at carnival',
+    link: '/services#unity-walk-carnival'
+  },
+  {
+    title: 'Tours',
+    img: tours,
+    alt: 'Lady sitting in car trunk reading a map',
+    link: '/services#tours'
+  }
 ]
