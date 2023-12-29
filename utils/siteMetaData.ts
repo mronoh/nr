30c1eca4499ee996532579d81957a0b959178964
Christian Onoh <chibyk5000@gmail.com>
2023-12-29 18:42:26 +0100

    Add site metadata configuration

---
+++ utils/siteMetaData.ts
@@ -0,0 +1,17 @@
+export const siteMetadata = {
+  title: 'Ngworocks',
+  author: 'Christian Onoh',
+  headerTitle: 'Ngworocks',
+  description: 'A blog created with Next.js, Tailwind.css and Sanity.',
+  language: 'en-us',
+  theme: 'system',
+  siteUrl: 'https://ngworocks.vercel.app',
+  siteLogo: '/images/logo.svg',
+  socialBanner: '/images/social_media_banner.png', // add social banner in the public folder
+  email: 'chibyk5000@gmail.com',
+  github: 'https://github.com/christianonoh',
+  twitter: 'https://twitter.com/christianonoh',
+  facebook: 'https://facebook.com',
+  linkedin: 'https://www.linkedin.com/in/christianonoh/',
+  locale: 'en-US'
+}
