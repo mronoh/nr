2e6ed2adbd73e477e4104f231a0ee8cf3b2a21a7
Christian Onoh <chibyk5000@gmail.com>
2023-12-29 13:37:59 +0100

    Refactor WhoAreWe component layout and add image

--- components/home/WhoAreWe.tsx
+++ components/home/WhoAreWe.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import Image from 'next/image'
+import WhoAreWeImg from '@/public/images/whoarewe.svg'
 
 const WhoAreWe = () => {
   return (
-    <section className='mx-auto w-full max-w-7xl px-5 py-24 pt-72 sm:px-10'>
-      <div className='flex'>
-        <div className='flex w-1/2 max-w-xl flex-col justify-center'>
+    <section className='mx-auto w-full max-w-7xl px-5 pt-64 sm:px-10'>
+      <div className='flex flex-col gap-8 md:flex-row'>
+        <div className='flex w-full max-w-xl flex-col justify-center md:w-1/2'>
           <h2 className='mb-12 text-4xl font-semibold text-dark'>
             Who we are?
           </h2>
@@ -15,6 +16,15 @@ const WhoAreWe = () => {
             engagement. Embracing heritage, we aspire to make Ngwo a vibrant
             tourism hub while inspiring startups across regions.
           </p>
+        </div>
+        <div className='flex w-full flex-col items-center md:w-1/2 md:px-8 lg:px-0'>
+          <Image
+            src={WhoAreWeImg}
+            alt='Who are we?'
+            priority
+            className='h-full w-full max-w-md object-contain'
+            sizes='(max-width: 768px) 100vw, 768px'
+          />
         </div>
       </div>
     </section>
