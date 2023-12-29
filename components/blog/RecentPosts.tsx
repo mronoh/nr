e871dd01e44142a7a4f8e4d4386f78311b8fc810
Christian Onoh <chibyk5000@gmail.com>
2023-12-29 18:34:30 +0100

    Update RecentPosts component styling

--- components/blog/RecentPosts.tsx
+++ components/blog/RecentPosts.tsx
@@ -11,10 +11,12 @@ const RecentBlogs = async () => {
   return (
     <section className='mx-auto w-full max-w-7xl px-5 pt-24 sm:px-10'>
       <div className='flex w-full items-center justify-between'>
-        <h2 className='mb-12 text-4xl font-semibold text-dark'>Recent Blogs</h2>
+        <h2 className='mb-8 text-4xl font-semibold  text-dark dark:text-light md:mb-12 lg:text-5xl'>
+          Recent Blogs
+        </h2>
         <Link
           href='/catergories/all'
-          className='rounded-full bg-accent/20 px-4 py-2 capitalize text-accent hover:text-light'
+          className='dark:text-accent-dark rounded-full bg-accent/20 px-4 py-2 capitalize text-accent hover:text-light'
         >
           view all
         </Link>
