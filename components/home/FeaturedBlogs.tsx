7187bd082061959f12863ebff905dfe3c3b44d6d
Christian Onoh <chibyk5000@gmail.com>
2023-12-31 10:27:56 +0100

    Refactor FeaturedBlogs component and add "View all blogs" button

--- components/home/FeaturedBlogs.tsx
+++ components/home/FeaturedBlogs.tsx
@@ -2,28 +2,15 @@ import { sanityFetch } from '@/sanity/lib/fetch'
 import BlogLayoutOne, { Blog, Post } from '../blog/BlogLayoutOne'
 import BlogLayoutTwo from '../blog/BlogLayoutTwo'
 import { cx } from '@/utils'
-import { SanityDocument } from 'next-sanity'
-import { postsQuery } from '@/sanity/lib/queries'
-
-export const dummyBlog: Blog = {
-  title: 'Sample Blog Post',
-  url: 'https://example.com/sample-blog-post',
-  publishedAt: '2023-11-30T12:00:00Z',
-  tags: ['technology', 'programming', 'web development'],
-  image: {
-    filePath: '/images/homeBgImg.png',
-    blurhashDataUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA...',
-    width: 1200,
-    height: 800
-  }
-}
+import Link from 'next/link'
+import Button from '../shared/Button'
 
 const FeaturedBlogs = async ({ posts = [] }: { posts: Post[] }) => {
   if (posts.length > 0) {
     return (
       <section className='mx-auto w-full max-w-7xl  px-5 pt-24 sm:px-10'>
         <div>
-          <h2 className='mb-8 text-4xl font-semibold  text-dark dark:text-light md:mb-12 lg:text-5xl'>
+          <h2 className='mb-8 text-3xl font-semibold text-dark  dark:text-light xs:text-4xl md:mb-12 lg:text-5xl'>
             Featured Blogs
           </h2>
           <div className={cx('grid grid-cols-2 grid-rows-2 gap-6')}>
@@ -37,6 +24,7 @@ const FeaturedBlogs = async ({ posts = [] }: { posts: Post[] }) => {
               <BlogLayoutOne post={posts[2]} />
             </article>
           </div>
+          <Button text="View all blogs" href="/tags/all" className='mt-8' />
         </div>
       </section>
     )
