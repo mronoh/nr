f09e163a463f24083975fd26762504b7f94a19ab
Christian Onoh <chibyk5000@gmail.com>
2024-01-01 16:46:56 +0100

    Update site metadata

--- utils/siteMetaData.ts
+++ utils/siteMetaData.ts
@@ -1,14 +1,14 @@
 export const siteMetadata = {
-  title: 'Ngworocks',
-  author: 'Christian Onoh',
+  title: 'NgwoRocks',
+  author: 'NgwoRocks',
   headerTitle: 'Ngworocks',
   description:
     'Ngworocks is a platform that promotes Tourism, unharnessed tourism potentials, culture & values.',
   language: 'en-us',
   theme: 'system',
   siteUrl: 'https://ngworocks.vercel.app',
-  siteLogo: '/images/logo.svg',
-  socialBanner: '/images/social_media_banner.png', // add social banner in the public folder
+  siteLogo: '/favicon/icon-192x192.png',
+  socialBanner: '/images/socialCover.webp', // add social banner in the public folder
   email: 'chibyk5000@gmail.com',
   github: 'https://github.com/christianonoh',
   facebook: 'https://web.facebook.com/NgwoROCK',
