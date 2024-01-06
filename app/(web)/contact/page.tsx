365b456d725833f7713659956d761c520dbb947b
Christian Onoh <chibyk5000@gmail.com>
2024-01-06 17:01:13 +0100

    Add contact page component

---
+++ app/(web)/contact/page.tsx
@@ -0,0 +1,28 @@
+import ContactForm from '@/components/contact/ContactForm'
+import { ContactImageStroke } from '@/components/icons'
+import contactImage from '@/public/images/contactImage.webp'
+import Image from 'next/image'
+
+const ContactPage = () => {
+  return (
+    <main className='mx-auto w-full max-w-7xl px-5 pt-16 sm:px-10 md:pt-24'>
+      <div className='border-b-2 border-dark dark:border-light'>
+        <h1 className='lg:text-5x mx-auto mb-6 max-w-4xl text-2xl font-semibold capitalize text-dark dark:text-light md:text-4xl'>
+          We'd love to hear your questions about our services, feel free to get
+          in touch.
+        </h1>
+      </div>
+
+      <section className='mt-6 flex flex-col sm:mt-8 md:mt-16 md:flex-row lg:gap-16'>
+        <div className='hidden w-full items-center justify-center md:flex md:w-3/5 lg:w-1/2'>
+          <ContactImageStroke className='flex w-full flex-1' />
+        </div>
+        <div className='w-full md:w-3/5 lg:w-1/2'>
+          <ContactForm />
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default ContactPage
