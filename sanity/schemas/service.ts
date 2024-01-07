e35918bd1cf141aaed6e3840a41514dc61f62744
Christian Onoh <chibyk5000@gmail.com>
2024-01-07 01:14:46 +0100

    Add gallery and service schemas

---
+++ sanity/schemas/service.ts
@@ -0,0 +1,75 @@
+import { defineField, defineType } from 'sanity'
+import { PublishIcon } from '@sanity/icons'
+
+export default defineType({
+  name: 'service',
+  title: 'Service',
+  type: 'document',
+  icon: PublishIcon,
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => [
+        Rule.required()
+          .min(2)
+          .error('A title of min. 2 characters is required'),
+        Rule.max(60).warning('Shorter titles are usually better')
+      ]
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 80
+      }
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      rows: 3,
+      description: 'Description of the service',
+      validation: Rule =>
+        Rule.required()
+          .min(50)
+          .max(140)
+          .warning('Should be under 140 characters')
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text'
+        }
+      ]
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent'
+    }),
+    defineField({
+      name: 'gallery',
+      title: 'Gallery',
+      type: 'gallery'
+    })
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage'
+    }
+  }
+})
