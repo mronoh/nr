dbb44dc2833137f67d615354d7b51bcc25daf6af
Christian Onoh <chibyk5000@gmail.com>
2024-01-10 20:53:29 +0100

    Refactor sanity.links.ts file

--- sanity/lib/sanity.links.ts
+++ sanity/lib/sanity.links.ts
@@ -1,24 +1,23 @@
-import { SanityDocument } from "sanity";
-import { previewSecretId } from "../env";
+import { SanityDocument } from 'sanity'
+import { previewSecretId } from '../env'
 
-const remoteUrl = `ngworocks.vercel.app`;
-const localUrl = `http://localhost:3000`;
+const remoteUrl = `ngworocks.vercel.app`
+const localUrl = `http://localhost:3000`
 
 export function resolveProductionUrl(doc: SanityDocument) {
   const baseUrl =
-    window.location.hostname === "localhost" ? localUrl : remoteUrl;
-  const previewUrl = new URL(baseUrl);
-  const slug: { current: string } = doc.slug as { current: string };
-  console.log(slug);
-  previewUrl.pathname = `/api/preview`;
-  previewUrl.searchParams.append(`type`, doc._type.toLowerCase());
-  previewUrl.searchParams.append(`secret`, previewSecretId!);
+    window.location.hostname === 'localhost' ? localUrl : remoteUrl
+  const previewUrl = new URL(baseUrl)
+  const slug: { current: string } = doc.slug as { current: string }
+  previewUrl.pathname = `/api/preview`
+  previewUrl.searchParams.append(`type`, doc._type.toLowerCase())
+  previewUrl.searchParams.append(`secret`, previewSecretId!)
   slug &&
     previewUrl.searchParams.append(
       `slug`,
-      `${slug.current ? slug.current : ""}`
-    );
-  return previewUrl.toString().replaceAll("%2F", "/");
+      `${slug.current ? slug.current : ''}`
+    )
+  return previewUrl.toString().replaceAll('%2F', '/')
 }
 
 export function resolveHref(
@@ -26,10 +25,10 @@ export function resolveHref(
   slug?: string
 ): string | undefined {
   switch (documentType) {
-    case "post":
-      return slug ? `/blog/${slug}` : "/";
+    case 'post':
+      return slug ? `/blog/${slug}` : '/'
     default:
-      console.warn("Invalid document type:", documentType);
-      return undefined;
+      console.warn('Invalid document type:', documentType)
+      return undefined
   }
 }
