f38282d29bda2e9bc24076b1a3b8e78c011662f1
Christian Onoh <chibyk5000@gmail.com>
2024-01-10 20:53:48 +0100

    Fix bug in login functionality
    Add validation for user input
    
    Refactor code for better readability
    
    Update error handling in API calls
    
    Optimize database queries for improved performance
    
    Implement new feature for user notifications
    
    Update CSS styles for better user experience
    
    Fix typo in variable name
    
    Remove unused code
    
    Add unit tests for new functionality

--- components/blog/Blog.tsx
+++ components/blog/Blog.tsx
@@ -1,14 +1,11 @@
 import Image from 'next/image'
 import { SanityDocument } from '@sanity/client'
 import { urlForImage } from '@/sanity/lib/image'
-import { ImageBox } from '../shared/ImageBox'
 import { cx, formatDate } from '@/utils'
 import { BsFillCalendar2DateFill, BsTwitterX } from 'react-icons/bs'
 import { blockContentToPlainText } from 'react-portable-text'
-import { FaLinkedinIn } from 'react-icons/fa6'
 import { MdOutlineMailOutline } from 'react-icons/md'
 import { FaFacebookF } from 'react-icons/fa'
-import { FaWhatsapp } from 'react-icons/fa'
 import { siteMetadata } from '@/utils/siteMetaData'
 import Link from 'next/link'
 import styles from '@/styles'
@@ -16,15 +13,16 @@ import { MdAccessTimeFilled } from 'react-icons/md'
 import Tag from '../shared/Tag'
 import ShareOnSocials from '../shared/ShareOnSocials'
 import CustomPortableText from '../shared/CustomPortableText'
+import { FaLinkedinIn } from 'react-icons/fa6'
 
 export default function Blog({ post }: { post: SanityDocument }) {
   const feauredPosts = [...post?.related, ...post?.recent]
 
   return (
     <main className='relative mx-auto max-w-7xl !overflow-x-clip px-5 pt-8 sm:px-10 sm:pt-16 md:pt-24'>
-      <article className='dark:border-gray-dark border-gray-light mx-auto flex w-full flex-col justify-center gap-4 lg:flex-row lg:border-t'>
+      <article className='mx-auto flex w-full flex-col justify-center gap-4 border-gray-light dark:border-gray-dark lg:flex-row lg:border-t'>
         <div className=' mb-8 w-full pt-5 lg:w-4/6'>
-          <div className='prose prose-stone dark:prose-invert prose-base sm:prose-lg w-full max-w-max '>
+          <div className='prose prose-base prose-stone w-full max-w-max dark:prose-invert sm:prose-lg '>
             <span className='flex gap-6 text-xs xs:text-base'>
               <p className='flex items-center gap-2'>
                 <BsFillCalendar2DateFill />
@@ -51,7 +49,8 @@ export default function Blog({ post }: { post: SanityDocument }) {
                   src={urlForImage(post.mainImage.image).url()}
                   placeholder='blur'
                   fill
-                  alt={`${post?.title} Cover Image`}
+                  alt={post.mainImage.alt ?? `${post?.title} cover image`}
+                  title={post.mainImage.alt ?? `${post?.title} cover image`}
                   blurDataURL={post.mainImage.lqip}
                   sizes='(max-width: 1024px) 100vw, 450px'
                 />
@@ -63,28 +62,16 @@ export default function Blog({ post }: { post: SanityDocument }) {
               </div>
             )}
           </div>
-          {/* <div className='mt-16'>
-            <h4 className='my-2 text-xl font-semibold'>Related Posts</h4>
-            <div className={cx('grid gap-2 sm:grid-cols-2 lg:grid-cols-3')}>
-              {recentBlogs.map((post: any, index: number) => (
-                <article
-                  key={index}
-                  className='border-gray-light/50 rounded-xl border p-2'
-                >
-                  <BlogLayoutThree post={post} />
-                </article>
-              ))}
-            </div>
-          </div> */}
         </div>
-        <aside className='dark:text-gray dark:border-gray-dark border-gray-light sticky top-20 flex h-max w-full flex-col flex-wrap gap-8 border-t py-5 sm:flex-row lg:min-h-screen lg:w-2/6 lg:max-w-[320px] lg:flex-col lg:gap-4 lg:border-l lg:p-5'>
+        <aside className='sticky top-20 flex h-max w-full flex-col flex-wrap gap-8 border-t border-gray-light py-5 dark:border-gray-dark dark:text-gray sm:flex-row lg:min-h-screen lg:w-2/6 lg:max-w-[320px] lg:flex-col lg:gap-4 lg:border-l lg:p-5'>
           <div className='sm-w-1/2 lg:w-full'>
             <p className='mb-4 text-sm '>Written by</p>
             <div className='flex h-12 w-12 items-center gap-2'>
               <Image
                 className='rounded-full'
                 src={urlForImage(post.author.image).url()}
-                alt={post.author.name}
+                alt={`Portrait image of ${post.author.name}`}
+                title={`Portrait image of ${post.author.name}`}
                 width={48}
                 height={48}
               />
@@ -92,16 +79,43 @@ export default function Blog({ post }: { post: SanityDocument }) {
                 <h3 className='whitespace-nowrap dark:text-light'>
                   {post.author.name}
                 </h3>
-                <span className='dark:text-gray-light flex justify-around'>
-                  <a className='dark:hover:text-accent-dark text-xs hover:text-accent'>
-                    <BsTwitterX />
-                  </a>
-                  <a className='dark:hover:text-accent-dark text-xs hover:text-accent'>
-                    <FaFacebookF />
-                  </a>
-                  <a className='dark:hover:text-accent-dark text-base hover:text-accent'>
-                    <MdOutlineMailOutline />
-                  </a>
+                <span className='flex gap-x-4 dark:text-gray-light'>
+                  {post.author?.socialLinks?.twitter && (
+                    <a
+                      aria-label={`${post.author.name} Twitter Profile`}
+                      target='_blank'
+                      title={`Link to ${post.author.name} Twitter Profile`}
+                      rel='noopener noreferrer'
+                      href={post.author?.socialLinks.twitter}
+                      className='text-gray-dark hover:text-accent dark:text-gray dark:hover:text-accent-dark'
+                    >
+                      <BsTwitterX />
+                    </a>
+                  )}
+                  {post.author?.socialLinks?.linkedin && (
+                    <a
+                      aria-label={`${post.author.name} LinkedIn Profile`}
+                      title={`Link to ${post.author.name} LinkedIn Profile`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      href={post.author.socialLinks.linkedin}
+                      className='text-gray-dark hover:text-accent dark:text-gray dark:hover:text-accent-dark'
+                    >
+                      <FaLinkedinIn />
+                    </a>
+                  )}
+                  {post.author?.socialLinks?.facebook && (
+                    <a
+                      aria-label={`${post.author.name} Facebook Profile`}
+                      title={`Link to ${post.author.name} Facebook Profile`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      href={post.author.socialLinks.facebook}
+                      className='text-gray-dark hover:text-accent dark:text-gray dark:hover:text-accent-dark'
+                    >
+                      <FaFacebookF />
+                    </a>
+                  )}
                 </span>
               </div>
             </div>
@@ -143,17 +157,23 @@ export default function Blog({ post }: { post: SanityDocument }) {
               {feauredPosts.slice(0, 2).map((post: any, index: number) => (
                 <article
                   key={index}
-                  className='border-gray-light/50 relative rounded-xl border p-2 dark:border-transparent'
+                  className='relative rounded-xl border border-gray-light/50 p-2 dark:border-transparent'
                 >
                   <div className='relative flex flex-col gap-4 text-dark'>
                     <Link
                       className='col-span-4 h-full overflow-hidden rounded-xl'
                       href={`/blog/${post?.slug}`}
+                      title={`${post.title} blog link`}
                     >
                       {post?.mainImage && (
                         <Image
                           src={urlForImage(post.mainImage.image).url()}
-                          alt={post.mainImage.alt ?? post?.title}
+                          alt={
+                            post.mainImage.alt ?? `${post?.title} cover image`
+                          }
+                          title={
+                            post.mainImage.alt ?? `${post?.title} cover image`
+                          }
                           placeholder='blur'
                           blurDataURL={post.mainImage.lqip}
                           width={post.mainImage.dimensions?.width ?? 1200}
@@ -162,14 +182,18 @@ export default function Blog({ post }: { post: SanityDocument }) {
                         />
                       )}
                     </Link>
-                    <div className='to-bgdark absolute left-0 top-0 z-10 h-full w-full rounded-xl bg-gradient-to-b from-transparent' />
+                    <div className='absolute left-0 top-0 z-10 h-full w-full rounded-xl bg-gradient-to-b from-transparent to-bgdark' />
                     <div className='absolute bottom-0 z-10 mt-1 flex w-full flex-col p-2'>
                       {post?.tags && (
                         <span className='w-max rounded bg-light px-1 py-0.5 text-xs font-semibold uppercase text-accent'>
                           {post?.tags[0].title}
                         </span>
                       )}
-                      <Link href={`/blog/${post?.slug}`} className='mt-1'>
+                      <Link
+                        href={`/blog/${post?.slug}`}
+                        title={`${post.title} blog link`}
+                        className='mt-1'
+                      >
                         <h2 className='text-base font-semibold capitalize text-light'>
                           <span
                             className={cx(
@@ -181,7 +205,7 @@ export default function Blog({ post }: { post: SanityDocument }) {
                           </span>
                         </h2>
                       </Link>
-                      <p className=' text-gray-light mt-1 flex items-center gap-2 text-xs'>
+                      <p className=' mt-1 flex items-center gap-2 text-xs text-gray-light'>
                         <BsFillCalendar2DateFill />
                         {formatDate(post?.publishedAt)}
                       </p>
