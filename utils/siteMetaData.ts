cf53e5e43ea7a1c43c403e131f44a2ef3b5172e8
Christian Onoh <chibyk5000@gmail.com>
2024-01-10 20:48:46 +0100

    Add SEO keywords to site metadata

--- utils/siteMetaData.ts
+++ utils/siteMetaData.ts
@@ -2,20 +2,39 @@ const remoteUrl = `https://ngworocks.com`
 const localUrl = `http://localhost:3000`
 const isDevelopment = process.env.NODE_ENV === 'development'
 const siteUrl = isDevelopment ? localUrl : remoteUrl
+
+const seoKeywords = [
+  'Ngworocks',
+  'Cultural Events',
+  'Tourism Hub',
+  'Community Engagement',
+  'Heritage Celebration',
+  'Vibrant Exploration',
+  'Young Creative Minds',
+  'Ngwo Startups',
+  'Regional Inspiration',
+  'Dynamic Endeavor',
+  'Ngwo Community',
+  'Ngwo',
+  'Pine Forest'
+]
 export const siteMetadata = {
   title: 'NgwoRocks',
   author: 'NgwoRocks',
   headerTitle: 'Ngworocks',
   description:
-    'Your platform for promoting untapped tourism potential, culture, and values.',
+    'Discover Ngworocks: A vibrant cultural and tourism hub. Join us on our dynamic journey, embracing heritage and inspiring startups. Your platform for promoting untapped tourism potential, culture, and values.',
   language: 'en-us',
   theme: 'system',
   siteUrl: siteUrl,
-  keywords: [],
+  keywords: seoKeywords,
   siteLogo: '/favicon/icon-192x192.png',
   socialBanner: '/images/socialCover.webp', // add social banner in the public folder
   email: 'chibyk5000@gmail.com',
-  github: 'https://github.com/christianonoh',
-  facebook: 'https://web.facebook.com/NgwoROCK',
+  social: {
+    instagram: 'https://instagram.com/ngworocks',
+    facebook: 'https://web.facebook.com/NgwoROCK',
+    mail: 'ngworocks@gmail.com'
+  },
   locale: 'en-US'
 }
