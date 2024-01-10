386c14eb178228fe89c96c66b73236102d236286
Christian Onoh <chibyk5000@gmail.com>
2024-01-10 20:49:14 +0100

    Refactor not-found component styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,8 +10,11 @@ export default function NotFound() {
       <Header isDraftMode={false} />
       <main className='relative -mb-24 flex h-screen items-center justify-center lg:max-h-[767px]'>
         <div className='relative flex flex-col items-center gap-8'>
-          <h1 className='z-50 text-xl text-center w-[80%] sm:text-3xl font-semibold text-red-500'> The requested page was not found 📖</h1>
-          <p className='dark:text-accent-dark text-center text-9xl sm:text-[12rem] font-bold text-accent'>
+          <h1 className='z-50 w-[80%] text-center text-xl font-semibold text-red-500 sm:text-3xl'>
+            {' '}
+            The requested page was not found 📖
+          </h1>
+          <p className='text-center text-9xl font-bold text-accent dark:text-accent-dark sm:text-[12rem]'>
             404
           </p>
           <span className='flex flex-col gap-4 sm:flex-row md:gap-8'>
@@ -23,16 +26,18 @@ export default function NotFound() {
             src='/images/birds.svg'
             width={80}
             height={80}
-            alt='Not found'
+            alt='Silloutte illustration of birds flying'
+            title='Silloutte illustration of birds flying'
             className='absolute -right-20 -top-full filter dark:invert lg:-top-20'
           />
         </div>
         <Image
           src='/images/404.svg'
-          className='absolute left-0 top-20 h-48 w-48 sm:h-64 sm:w-64 md:h-auto md:w-auto opacity-70 dark:opacity-100'
+          className='absolute left-0 top-20 h-48 w-48 opacity-70 dark:opacity-100 sm:h-64 sm:w-64 md:h-auto md:w-auto'
           width={400}
           height={400}
-          alt='Not found'
+          alt='Silloutte illustration of man hanging from a cliff'
+          title='Silloutte illustration of man hanging from a cliff'
           sizes='(max-width: 639px) 50vw, 400px'
         />
       </main>
