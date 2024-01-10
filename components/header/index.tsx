ea97f31c79e3e1201ee3369df7b2dce6be0c4a9f
Christian Onoh <chibyk5000@gmail.com>
2024-01-10 21:14:08 +0100

    Add social media links to header component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -11,6 +11,8 @@ import { PreviewBanner } from '../preview/PreviewBanner'
 import { useThemeSwitch } from '../hooks/useThemeSwitch'
 import { Hamburger, ThemeButton } from '../icons'
 import Button from '../shared/Button'
+import { siteMetadata } from '@/utils/siteMetaData'
+import { FaFacebookF, FaInstagram } from 'react-icons/fa'
 
 const Header = ({ isDraftMode }: { isDraftMode: boolean }) => {
   const [mode, setMode] = useThemeSwitch()
@@ -52,19 +54,21 @@ const Header = ({ isDraftMode }: { isDraftMode: boolean }) => {
         isSticky
           ? 'sticky top-0 translate-y-0 transform shadow'
           : '-translate-y-full transform',
-        'dark:bg-bgdark/80 relative z-30 block w-full bg-light/80 backdrop-blur-lg transition-transform duration-500 ease-in-out'
+        'relative z-30 block w-full bg-light/80 backdrop-blur-lg transition-transform duration-500 ease-in-out dark:bg-bgdark/80'
       )}
     >
       {isDraftMode && <PreviewBanner />}
       <nav className='mx-auto flex max-w-7xl items-center justify-between px-5 py-2 text-base font-semibold capitalize  text-dark dark:text-light xs:py-6 sm:px-10'>
         <Link
           href='/'
+          title='Link to homepage'
           onClick={toggled ? handleToggle : () => {}}
           className='z-50 flex h-12 w-12 justify-center lg:absolute lg:right-1/2 lg:h-16 lg:w-16 lg:translate-x-1/2'
         >
           <Image
             src={Logo}
-            alt='Christian Onoh'
+            alt='NgwoRocks logo image'
+            title='NgwoRocks logo image'
             width={64}
             height={64}
             priority
@@ -77,9 +81,10 @@ const Header = ({ isDraftMode }: { isDraftMode: boolean }) => {
             <Link
               key={index}
               href={link.url}
+              title={`Link to ${link.name}`}
               className={cx(
                 pathname === link.url
-                  ? 'dark:bg-accent-dark/10 bg-accent/10'
+                  ? 'bg-accent/10 dark:bg-accent-dark/10'
                   : 'text-dark dark:text-light',
                 'rounded-full px-4 py-1.5 transition-all duration-200 ease-in-out hover:scale-105 hover:bg-accent/10'
               )}
@@ -95,9 +100,10 @@ const Header = ({ isDraftMode }: { isDraftMode: boolean }) => {
             <Link
               key={index}
               href={link.url}
+              title={`Link to ${link.name}`}
               className={cx(
                 pathname === link.url
-                  ? 'dark:bg-accent-dark/10 bg-accent/10'
+                  ? 'bg-accent/10 dark:bg-accent-dark/10'
                   : 'text-dark dark:text-light',
                 'rounded-full px-4 py-1.5 transition-all duration-200 ease-in-out hover:scale-105 hover:bg-accent/10'
               )}
@@ -106,6 +112,28 @@ const Header = ({ isDraftMode }: { isDraftMode: boolean }) => {
             </Link>
           ))}
           <Button text='get in touch' href='/contact' />
+          <span className='flex gap-2'>
+            <a
+              aria-label='NgwoRocks facebook profile link'
+              title='NgwoRocks facebook profile link'
+              target='_blank'
+              rel='noopener noreferrer'
+              href={siteMetadata.social.facebook}
+              className='text-xl text-gray-dark hover:text-accent dark:text-gray dark:hover:text-accent-dark '
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              aria-label='NgwoRocks instagram profile link'
+              title='NgwoRocks instagram profile link'
+              target='_blank'
+              rel='noopener noreferrer'
+              href={siteMetadata.social.instagram}
+              className='text-xl text-gray-dark hover:text-accent dark:text-gray dark:hover:text-accent-dark'
+            >
+              <FaInstagram />
+            </a>
+          </span>
           <ThemeButton mode={mode} setMode={setMode} />
         </div>
 
@@ -113,6 +141,7 @@ const Header = ({ isDraftMode }: { isDraftMode: boolean }) => {
         <div className='flex items-center gap-3 xs:gap-6 lg:hidden'>
           <Link
             href='/blog'
+            title='Link to Blogs Page'
             onClick={toggled ? handleToggle : () => {}}
             className='z-50 rounded-full px-4 py-1.5 transition-all duration-200 ease-in-out hover:scale-105 hover:bg-accent/10'
           >
@@ -126,7 +155,7 @@ const Header = ({ isDraftMode }: { isDraftMode: boolean }) => {
           <div
             className={cx(
               toggled ? 'origin-top scale-y-100' : 'origin-top scale-y-0',
-              'dark:bg-bgdark absolute left-0 top-0 z-30 flex h-[384px] w-full transform flex-col justify-center bg-white shadow-sm transition-transform duration-300 ease-in-out lg:hidden'
+              'absolute left-0 top-0 z-30 flex h-[384px] w-full transform flex-col justify-center bg-white shadow-sm transition-transform duration-300 ease-in-out dark:bg-bgdark lg:hidden'
             )}
           >
             <div className='mx-auto flex h-full w-full max-w-[287px] flex-col items-center gap-4 px-8 py-32 text-base font-semibold capitalize text-dark dark:text-light'>
@@ -134,8 +163,9 @@ const Header = ({ isDraftMode }: { isDraftMode: boolean }) => {
                 <Link
                   key={index}
                   href={link.url}
+                  title={`Link to ${link.name}`}
                   onClick={handleToggle}
-                  className='dark:hover:bg-accent-dark/10  rounded-full px-4 py-1.5 text-center transition-all duration-200 ease-in-out hover:scale-105 hover:bg-accent/10 '
+                  className='rounded-full  px-4 py-1.5 text-center transition-all duration-200 ease-in-out hover:scale-105 hover:bg-accent/10 dark:hover:bg-accent-dark/10 '
                 >
                   {link.name}
                 </Link>
