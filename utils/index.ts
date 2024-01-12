dc51ca5005fe506561a01f6864ad4ce0007f53f6
Christian Onoh <chibyk5000@gmail.com>
2024-01-12 18:36:58 +0100

    Update date format in BlogOgImage component

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,20 @@
-import { compareDesc, parseISO, format } from 'date-fns'
-
 export const cx = (...classNames: (string | undefined)[]) => {
   return classNames.filter(Boolean).join(' ')
 }
 
-export const formatDate = (date: string, dateFormat = 'MMMM dd, yyyy') => {
-  return format(parseISO(date), dateFormat)
+export function formatDate(
+  datetimeString: string,
+  format: 'long' | 'short' = 'long'
+): string {
+  const options: Intl.DateTimeFormatOptions = {
+    month: format === 'long' ? 'long' : 'short',
+    day: 'numeric',
+    year: 'numeric'
+  }
+  const formattedDate: string = new Date(datetimeString).toLocaleDateString(
+    'en-US',
+    options
+  )
+
+  return formattedDate
 }
