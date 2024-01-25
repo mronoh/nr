7eeec29c656e72525a789a2c221f89d80a4b2062
Christian Onoh <chibyk5000@gmail.com>
2024-01-26 00:26:29 +0100

    Add link to Blog Documentation in StudioNavbar

--- components/shared/StudioNavbar.tsx
+++ components/shared/StudioNavbar.tsx
@@ -1,16 +1,29 @@
 import { IoMdArrowRoundBack } from 'react-icons/io'
+import { SiGoogledocs } from 'react-icons/si'
 import Link from 'next/link'
 
 const StudioNavbar = (props: any) => {
   return (
     <div>
-      <Link
-        href='/'
-        className='m-1 flex w-fit items-center gap-1 rounded bg-light/10 p-1 text-sm text-accent-dark'
-      >
-        <IoMdArrowRoundBack className='text-base' />
-        Back to Website
-      </Link>
+      <div className='flex justify-between'>
+        <Link
+          href='/'
+          className='m-1 flex w-fit items-center gap-1 rounded bg-light/10 p-1 text-sm text-accent-dark'
+        >
+          <IoMdArrowRoundBack className='text-base' />
+          Back to Website
+        </Link>
+        <a
+          href='https://nrblogdocs.my.canva.site/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='m-1 flex w-fit items-center gap-1 rounded bg-light/10 p-1 text-sm text-accent-dark'
+        >
+          <SiGoogledocs className='text-base' />
+          Blog Documentation
+        </a>
+      </div>
+
       <>{props.renderDefault(props)}</>
     </div>
   )
